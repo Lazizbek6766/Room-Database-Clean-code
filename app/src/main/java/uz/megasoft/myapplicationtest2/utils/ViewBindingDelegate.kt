6646c1f27f304a6f65a36a3ac9ec3aa65da5c68b
Lazizbek6766 <turayevlazizbek6766@gmail.com>
2023-10-06 11:34:57 +0500

    view binding

--- app/src/main/java/uz/megasoft/myapplicationtest2/utils/ViewBindingDelegate.kt
+++ app/src/main/java/uz/megasoft/myapplicationtest2/utils/ViewBindingDelegate.kt
@@ -14,7 +14,6 @@ import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
 /** Activity binding delegate, may be used since onCreate up to onDestroy (inclusive) */
-
 inline fun <T : ViewBinding> AppCompatActivity.viewBinding(crossinline factory: (LayoutInflater) -> T) =
     lazy(LazyThreadSafetyMode.NONE) {
         factory(layoutInflater)
